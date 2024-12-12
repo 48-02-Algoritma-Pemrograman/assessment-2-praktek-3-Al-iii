234c44259e74d065fdd4abd1499ca21e48575894
Al-iii <anh191204@gmail.com>
2024-12-12 13:12:44 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,12 +1,23 @@
-import java.util.Scanner;
-
-public class Nomor1 {
-
+static String nPrint(int n) {
+        if (n == 0) {
+            return String.valueOf(n);
+        } else if (n % 3 == 0 && n % 5 == 0) {
+            return "FizzBuzz";
+        } else if (n % 3 == 0) {
+            return "Fizz";
+        } else if (n % 5 == 0) {
+            return "Buzz";
+        } 
+        return String.valueOf(n);
+    }
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
-
+        for (int i =0;i<=N;i++){
+            System.out.print(nPrint(i)+" ");
+        }
+        input.close();
         /* Pekerjaan anda berakhir sini */
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
